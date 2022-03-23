54ffe649e85d8c52f963f899cbfd5b4a070fd9ee
NatBelova <n.belova@propellerads.net>
2022-03-23 13:16:18 +0300

    new method EnumUtils

--- src/main/java/animal/data/AnimalType.java
+++ src/main/java/animal/data/AnimalType.java
@@ -1,17 +1,7 @@
 package animal.data;
 
 public enum AnimalType {
-    CAT("CAT"),
-    DOG("DOG"),
-    DUCK("DUCK");
-
-    private final String name;
-
-    public String getAnimalType() {
-        return name;
-    }
-    AnimalType(String name) {
-        this.name = name;
-    }
-
+    CAT,
+    DOG,
+    DUCK
 }
